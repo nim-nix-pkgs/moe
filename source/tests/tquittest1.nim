46f57b18d7f8c53db87f681919dcbdb81e0173c7
hugosenari <hugosenari@gmail.com>
2022-09-03 19:49:47 -0300

    feat(nimpkgs): update lock

--- source/tests/tquittest1.nim
+++
@@ -1,9 +0,0 @@
-import std/unittest
-import moepkg/[editorstatus, unicodeext, exmode]
-
-test "Quit command":
-  var status = initEditorStatus()
-  status.addNewBuffer
-
-  const command = @[ru"q"]
-  status.exModeCommand(command, 100, 100)
